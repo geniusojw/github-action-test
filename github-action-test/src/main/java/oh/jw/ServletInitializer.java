8e72bcf6521079dd6570e1393353a5ec7f66ff60
jerri <jerri@DESKTOP-DDCLRF6>
2021-03-21 01:01:31 +0900

    test

---
+++ github-action-test/src/main/java/oh/jw/ServletInitializer.java
@@ -0,0 +1,13 @@
+package oh.jw;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GithubActionTestApplication.class);
+	}
+
+}
